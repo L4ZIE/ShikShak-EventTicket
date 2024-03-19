7e8044088c47b894d7518b407f62714654d38702
L4ZIE <davidwymon@yahoo.com>
2024-03-19 16:20:04 +0100

    added dataBaseConnector

--- src/dal/connector/dataBaseConnector.java
+++ src/dal/connector/dataBaseConnector.java
@@ -1,4 +1,38 @@
 package dal.connector;
 
-public class dataBaseConnector {
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class dataBaseConnector
+{
+    private SQLServerDataSource dataSource;
+
+    public dataBaseConnector()
+    {
+        dataSource = new SQLServerDataSource();
+        dataSource.setDatabaseName("CSe2023b_e_19-EventTicket");
+        dataSource.setUser("CSe2023b_e_19");
+        dataSource.setPassword("CSe2023bE19#23 ");
+        dataSource.setServerName("EASV-DB4");
+        dataSource.setPortNumber(1433);
+        dataSource.setTrustServerCertificate(true);
+    }
+
+    public Connection getConnection() throws SQLServerException
+    {
+        return dataSource.getConnection();
+    }
+
+    public static void main(String[] args) throws SQLException
+    {
+        dataBaseConnector dataBaseConnector = new dataBaseConnector();
+
+        try(Connection connection = dataBaseConnector.getConnection())
+        {
+            System.out.println("is it open?" + !connection.isClosed());
+        }
+    }
 }
