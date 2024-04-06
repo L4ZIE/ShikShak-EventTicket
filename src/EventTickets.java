8267c901d1b8fd2012d216be67a69a946f26e201
salmacx <145067369+salmacx@users.noreply.github.com>
2024-04-06 15:55:33 +0200

    added css files, and restored both initial fxml files

---
+++ src/EventTickets.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class EventTickets extends Application {
+
+    private double x = 0;
+    private double y = 0;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/view/login.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            String css = getClass().getResource("/gui/view/loginDesign.css").toExternalForm();
+            scene.getStylesheets().add(css);
+
+            root.setOnMousePressed(event -> {
+                x = event.getSceneX();
+                y = event.getSceneY();
+            });
+
+            root.setOnMouseDragged(event -> {
+                primaryStage.setX(event.getScreenX() - x);
+                primaryStage.setY(event.getScreenY() - y);
+
+                primaryStage.setOpacity(.8);
+            });
+
+            root.setOnMouseReleased(event -> {
+                primaryStage.setOpacity(1);
+            });
+
+
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception gracefully, maybe show an error dialog or log it.
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
