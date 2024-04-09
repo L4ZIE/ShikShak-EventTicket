adb7ada3f151c205af7f90322de274ffe63f540c
natalialukacova <natalialukacova11@gmail.com>
2024-04-09 23:01:46 +0200

    event files added

---
+++ src/gui/controller/NewEventController.java
@@ -0,0 +1,11 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+
+public class NewEventController {
+    public void closeWindow(ActionEvent event) {
+    }
+
+    public void createEvent(ActionEvent event) {
+    }
+}
